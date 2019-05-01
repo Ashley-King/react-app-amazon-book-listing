af3147bd67e0de361d81d20f3a9371c2da9187be
Ashley King <ashleyot@gmail.com>
2019-05-01 16:56:29 -0400

    working on css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Books = () => (
+ <section className='books'>
+   <Book/>
+   <Book/>
+   <Book/>
+   <Book/>
+   <Book/>
+   <Book/>
+ </section>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Book = () => {
+ return (
+  <article className='book'>
+   <CoverImage/>
+   <Title/>
+   <Author/>
+  </article>
+ )
+}
+
+const CoverImage = () => (
+ <img 
+  width='200px'
+  src="https://m.media-amazon.com/images/I/71N4oeWwYlL._AC_UL436_.jpg" alt="Wonky Donkey Pic"/>
+)
+
+const Title = () => (
+ <h1 style={{fontSize: "2rem", color: 'red'}}>The Wonky Donkey</h1>
+)
+
+const Author = () => (
+ <p>by Craig Smith </p>
+)
+
+ReactDOM.render(
+ <Books/>,
+ document.querySelector('#root')
+)
